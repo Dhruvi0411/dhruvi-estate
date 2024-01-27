fbbfa8bc4875dce6c6e9ca01c3b3ea8732adf313
SimbaDhruvi <d.dhameliya1101@gmail.com>
2024-01-27 15:04:42 +0530

    update listing Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return <Router>
@@ -19,6 +20,7 @@ export default function App() {
       <Route element={<PrivateRoute></PrivateRoute>}>
         <Route path='/profile' element={<Profile></Profile>}></Route>
         <Route path='/create-listing' element={<CreateListing></CreateListing>}></Route>
+        <Route path='/update-listing/:listingId' element={<UpdateListing></UpdateListing>}></Route>
       </Route>
     </Routes>
   </Router>
