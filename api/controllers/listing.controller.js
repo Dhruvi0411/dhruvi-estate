df02849b520160b1219cdb806efb2206a6be7346
SimbaDhruvi <d.dhameliya1101@gmail.com>
2024-01-27 09:38:11 +0530

    create listing Route in api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -18,12 +18,31 @@ export const deleteListing = async (req, res, next) => {
     }
 
     if (req.user.id !== listing.userRef) {
-        return next(errorHandler(401, 'You can only delete your own listing!'))
+        return next(errorHandler(401, 'Please Relogin in your account'))
     }
 
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Listing has been delete!')
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not Found!'))
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'Please Relogin in your account'))
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body,{new: true})
+        
     } catch (error) {
         next(error)
     }
